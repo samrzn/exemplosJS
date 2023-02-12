898625ac50882d7ebeb997810e22ea121597ce80
Samuel Rezende <sam_profissional@outlook.com>
2023-02-12 07:53:16 -0300

    algorithm function implantantion tests

--- test1.js
+++ test1.js
@@ -19,8 +19,8 @@ function json(ipgc_testSheet) {
     const sheet = spreadsheet.getSheetByName('ipgc_testSheet')
     const data = sheet.getDataRange().getValues()
     const jsonData = convertToJson(data)
-    const blankFilter = removeNullValues(blankFilter)
-    const finalJson = blankFilter
+    // const blankFilter = removeNullValues(blankFilter)
+    // const finalJson = blankFilter
     return ContentService
         .createTextOutput(JSON.stringify(jsonData))
         .setMimeType(ContentService.MimeType.JSON)
@@ -30,15 +30,47 @@ function convertToJson(data) {
     const headers = data[0]
     const raw_data = data.slice(1,)
     let json = []
+    let copy = []
     raw_data.forEach(d => {
         let object = {}
         for (let i = 0; i < headers.length; i++) {
             object[headers[i]] = d[i]
         }
-        json.push(object)
+        copy.push(object)
+        return copy;
     });
-    return json
+    function removeNull(nullValue) {
+        copy = copy.filter((isNull) => {
+            if (isNull !== false || isNull !== null || isNull !== 0 || isNull !== "")
+                return isNull;
+        });
+        return nullValue;
+    }
+    json.push(removeNull)
+    return json;
 }
+
+// function convertToJson(data) {
+//     const headers = data[0]
+//     const raw_data = data.slice(1,)
+//     let copy = []
+//     let json = []
+//     raw_data.forEach(d => {
+//         let object = {}
+//         for (let i = 0; i < headers.length; i++) {
+//             object[headers[i]] = d[i]
+//         }
+//         copy.push(object)
+//     });
+//     for (let y = 0; y < copy.length; y++) {
+//         if (copy !== "") {
+//             json.push(copy);
+//         }
+//     }
+//     return json
+// }
+
+copy[y] !== false || copy[y] !== null || copy[y] !== 0 || copy[y] !== ""
 
 function removeNullValues(blankFilter) {
     const safeCopy = blankFilter[0]
@@ -67,6 +99,36 @@ function json(ipgc_testSheet) {
         .createTextOutput(JSON.stringify(jsonData))
         .setMimeType(ContentService.MimeType.JSON)
 }
+ 
+function convertToJson(data) {
+    const headers = data[0]
+    const raw_data = data.slice(1,)
+    let json = []
+    raw_data.forEach(d => {
+        let object = {}
+        for (let i = 0; i < headers.length; i++) {
+            object[headers[i]] = d[i]
+        }
+        json.push(object)
+    });
+    return json
+}
+ 
+function doGet(e) {
+    const path = e.parameter.path
+    return json(path)
+} 
+*/
+
+function json(ipgc_testSheet) {
+    const spreadsheet = SpreadsheetApp.getActiveSpreadsheet()
+    const sheet = spreadsheet.getSheetByName('ipgc_testSheet')
+    const data = sheet.getDataRange().getValues()
+    const jsonData = convertToJson(data)
+    return ContentService
+        .createTextOutput(JSON.stringify(jsonData))
+        .setMimeType(ContentService.MimeType.JSON)
+}
 
 function convertToJson(data) {
     const headers = data[0]
@@ -86,4 +148,3 @@ function doGet(e) {
     const path = e.parameter.path
     return json(path)
 } 
-*/
